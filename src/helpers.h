117f30c0ab3bc46a0b24da0c7cbb29bd314cfc17
Lauri Kasanen <curaga@operamail.com>
2014-01-17 17:40:17 +0200

    Add a helper include

---
+++ src/helpers.h
@@ -0,0 +1,34 @@
+/*
+Copyright (C) 2013 Lauri Kasanen
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef HELPERS_H
+#define HELPERS_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+
+static inline void die(const char fmt[], ...) {
+
+	va_list ap;
+
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+
+	exit(1);
+}
+
+#endif
