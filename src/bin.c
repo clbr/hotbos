472831d2e9aaaaf9edd51ded7aeb273c077692de
Lauri Kasanen <curaga@operamail.com>
2014-01-18 19:10:42 +0200

    Support the new relative time format on read side too

--- src/bin.c
+++ src/bin.c
@@ -21,12 +21,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 void readentry(entry * const e, void * const in, const u8 charbufs) {
 
+	static u32 lasttime = 0;
 	u32 tmp = 0;
+
 	memset(e, 0, sizeof(entry));
 
-	sgzread(&tmp, 3, in);
-	e->id = (tmp >> 21) & 7;
-	e->time = tmp & 0x1fffff;
+	sgzread(&tmp, 1, in);
+	e->id = (tmp >> 5) & 7;
+	const u32 reltime = (tmp & 0x1f);
+	e->time = reltime + lasttime;
+	lasttime += reltime;
 
 	if (charbufs)
 		sgzread(&e->buffer, 1, in);
