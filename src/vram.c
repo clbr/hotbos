00046d87aa53ecc792613bb9d6d7690ced3e746c
Lauri Kasanen <curaga@operamail.com>
2014-01-20 16:30:25 +0200

    More vram work

--- src/vram.c
+++ src/vram.c
@@ -15,13 +15,45 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "vram.h"
+#include "helpers.h"
+
+struct buf {
+	u32 id;
+	u32 size;
+	struct buf *next;
+};
+
+static struct {
+	u32 size;
+	u32 edge;
+
+	struct buf *vram;
+	struct buf *ram;
+} ctx;
 
 void initvram(const u32 size, const u32 edge) {
 
+	ctx.size = size;
+	ctx.edge = edge;
 }
 
 void freevram() {
 
+	ctx.size = ctx.edge = 0;
+
+	struct buf *cur = ctx.vram;
+	while (cur) {
+		struct buf *next = cur->next;
+		free(cur);
+		cur = next;
+	}
+
+	cur = ctx.ram;
+	while (cur) {
+		struct buf *next = cur->next;
+		free(cur);
+		cur = next;
+	}
 }
 
 void allocbuf(const u32 id, const u32 size) {
