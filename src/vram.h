9ec74396cb8496f6dd49b12d6b88f0d71866b487
Lauri Kasanen <curaga@operamail.com>
2014-01-20 15:41:41 +0200

    Start on vram.h

---
+++ src/vram.h
@@ -0,0 +1,25 @@
+/*
+Copyright (C) 2013 Lauri Kasanen
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef VRAM_H
+#define VRAM_H
+
+#include "lrtypes.h"
+
+static const u32 vramsizes[] = { 64, 128, 256, 384, 512, 1024, 1536, 2048, 4096 };
+static const u32 vramelements = sizeof(vramsizes) / sizeof(u32);
+
+#endif
