e4da0f443b1d86f625e26b1312286064508f5dfd
Lauri Kasanen <curaga@operamail.com>
2014-01-21 18:47:19 +0200

    Add second edit round to filter-frag

--- src/filter-frag.sh
+++ src/filter-frag.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-awk 'prev !~ /swap/ || prev != $0; {prev = $0}'
+# First round
+#awk 'prev !~ /swap/ || prev != $0; {prev = $0}'
+
+# Second round
+sed -e 's@Fragments: @@' -e 's@[[:digit:]]$@&, 0@' -e 's@Fragmentation caused a swap@swap@' |
+	awk 1 RS="\nswap" ORS="s" | sed 's@0s@1@'
