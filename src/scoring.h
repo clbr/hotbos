d520c3268e850f22bf11c64ef1e62080d73913f1
Lauri Kasanen <curaga@operamail.com>
2014-01-27 17:55:24 +0200

    Declare it a const func

--- src/scoring.h
+++ src/scoring.h
@@ -17,6 +17,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef SCORING_H
 #define SCORING_H
 
+#include "lrtypes.h"
+#include "macros.h"
+
 enum {
 	SCORE_PCIE_LATENCY = 30000, // https://devtalk.nvidia.com/default/topic/485590/pci-express-latency-and-how-to-decrease-it/
 	SCORE_RAM_MULTIPLIER = 18, // 232 / 12.8 GB/s scale difference
@@ -39,5 +42,8 @@ enum scoreop_t {
 	SCORE_W,
 	SCORE_MOVE
 };
+
+u32 score(const enum scoredev_t dev, const enum scoreop_t op,
+		const enum scoredest_t dest, const u32 size) CONST_FUNC;
 
 #endif
