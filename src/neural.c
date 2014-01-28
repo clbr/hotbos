a4a7ef310b388635f5ee08b63bc50ec280224ce3
Lauri Kasanen <curaga@operamail.com>
2014-01-28 14:49:09 +0200

    Implement the neural network code

--- src/neural.c
+++ src/neural.c
@@ -24,10 +24,44 @@
 
 #include "neural.h"
 #include <stdint.h>
+#include <limits.h>
 
-void calculate_score(const float inputs[INPUT_NEURONS], const struct network * const net) {
+static float clampf(const float in, const float min, const float max) {
+	return in > max ? max : in < min ? min : in;
+}
 
-	uint32_t i;
+static float smootherstep(float x) {
+	x = clampf(x, 0, 1);
+
+	return x*x*x*(x*(x*6 - 15) + 10);
+}
+
+uint32_t calculate_score(const float inputs[INPUT_NEURONS], const struct network * const net) {
+
+	uint32_t i, j;
 	float input_results[INPUT_NEURONS], hidden_results[INPUT_NEURONS];
 
+	for (i = 0; i < INPUT_NEURONS; i++) {
+		input_results[i] = smootherstep(net->input[i].weight * inputs[i] +
+					net->input[i].bias);
+	}
+
+	/* Hidden layer */
+	for (i = 0; i < INPUT_NEURONS; i++) {
+		float weighted_sum = net->hidden[i].bias;
+		for (j = 0; j < INPUT_NEURONS; j++) {
+			weighted_sum += net->hidden[i].weights[j] * input_results[j];
+		}
+		hidden_results[i] = smootherstep(weighted_sum);
+	}
+
+	/* Output layer */
+	float weighted_sum = net->output.bias;
+	for (i = 0; i < INPUT_NEURONS; i++) {
+		weighted_sum += net->output.weights[i] * hidden_results[i];
+	}
+
+	const float score = clampf(smootherstep(weighted_sum), 0, 1);
+
+	return score * UINT_MAX;
 }
